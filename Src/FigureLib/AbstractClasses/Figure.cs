c40b3c10842b90fc5fe5638eaf47428b6e5ff61a
EdGudz <grin_edik@mail.ru>
2024-08-30 09:36:53 +0300

    Add test project

---
+++ Src/FigureLib/AbstractClasses/Figure.cs
@@ -0,0 +1,39 @@
+﻿using FigureLib.Interfaces;
+
+namespace FigureLib.AbstractClasses;
+
+public abstract class Figure : IFigure
+{
+    /// <summary>
+    /// Коллекция сторон фигуры
+    /// </summary>
+    public List<double> Sides { get; private set; } = new List<double>();
+
+    public Figure(List<double> sides)
+    {
+        if (ValidateSides(sides))
+        {
+            Sides.AddRange(sides);
+        }
+    }
+
+    /// <summary>
+    /// Метод смены сторон фигуры
+    /// </summary>
+    /// <param name="sides">Стороны фигуры</param>
+    public virtual void SetSides(List<double> sides)
+    {
+        if (ValidateSides(sides))
+        {
+            Sides.Clear();
+            sides.AddRange(sides);
+        }
+    }
+
+    /// <summary>
+    /// Функция валидация фигуры
+    /// </summary>
+    /// <param name="sides">Передаваемые стороны для проверки</param>
+    /// <returns>Флаг валидации</returns>
+    private protected abstract bool ValidateSides(List<double> sides);
+}
---
+++ Src/FigureLib/Services/Factory/FigureFactory.cs
@@ -0,0 +1,21 @@
+﻿using FigureLib.AbstractClasses;
+using FigureLib.Interfaces;
+using FigureLib.Models;
+
+namespace FigureLib.Services.Factory;
+
+public class FigureFactory : IFigureFactory
+{
+    public Figure CreateFigure(List<double> sides)
+    {
+        switch (sides.Count)
+        {
+            case 1:
+                return new Circle(sides);
+            case 3:
+                return new Triangle(sides);
+            default:
+                throw new ArgumentException("Нет типа фигур с количеством переданных сторон");
+        }
+    }
+}
